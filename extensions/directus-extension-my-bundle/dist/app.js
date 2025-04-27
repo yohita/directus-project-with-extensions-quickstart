dd4773d7ec919555c3f83b76d4ae0523863af102
Amit kulkarni <kulkarni005@gmail.com>
2025-04-27 13:49:07 +0530

    Ex

--- extensions/directus-extension-my-bundle/dist/app.js
+++ extensions/directus-extension-my-bundle/dist/app.js
@@ -1,1 +1,1 @@
-const o=[],t=[],c=[],e=[],n=[],p=[];export{t as displays,o as interfaces,c as layouts,e as modules,p as operations,n as panels};
+const o=[],t=[],c=[],e=[],n=[],p=[],r=[];export{t as displays,o as interfaces,c as layouts,e as modules,r as operations,n as panels,p as themes};
